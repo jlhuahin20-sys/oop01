60fb2467a4cc5eba313ec8671038d1ee73138b98
jlhuahin20-sys <jlhuahin20@gmail.com>
2026-06-17 16:50:55 +0700

    ส่งงาน Lab1_4

---
+++ Lab1_4.ts
@@ -0,0 +1,11 @@
+let Gross: number = 50000;
+let taxpay: number=Gross*0.07;
+let aftertax: number= Gross-taxpay;
+
+
+console.log("--- tax calculaton---");
+console.log("Gross incom :%d", Gross);
+console.log("Tax Rate: 7%");
+console.log("Tax Paid: %d",taxpay);
+console.log("Net income after tax: %d",aftertax);
+console.log("--------------------------------------");
